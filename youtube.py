b055a1b18415c4aba51ec18321b302bfa3618666
Charles Xia <charlesxsh@hotmail.com>
2015-11-22 15:31:54 -0500

    basic setup

--- youtube.py
+++ youtube.py
@@ -10,6 +10,7 @@ from oauth2client.client import OAuth2WebServerFlow
 from oauth2client.file import Storage
 from oauth2client.tools import argparser, run_flow
 import webbrowser
+
 YOUTUBE_READ_WRITE_SCOPE = "https://www.googleapis.com/auth/youtube"
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
@@ -28,6 +29,40 @@ def get_authenticated_service():
   return build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
     http=credentials.authorize(httplib2.Http()))
 
+# @description search by keyword
+# @param:youtube authenticated object <- created by get_authenticated_service()
+# @param:keyword keyword used to search 
+def searchByKeyword(youtube, keyword):
+  search_response = youtube.search().list(q="wpi",part="id,snippet",maxResults=25).execute()
+
+  videos = []
+  channels = []
+  playlists = []
+
+  # Add each result to the appropriate list, and then display the lists of
+  # matching videos, channels, and playlists.
+  for search_result in search_response.get("items", []):
+    if search_result["id"]["kind"] == "youtube#video":
+      videos.append("%s (%s)" % (search_result["snippet"]["title"],
+                                 search_result["id"]["videoId"]))
+    elif search_result["id"]["kind"] == "youtube#channel":
+      channels.append("%s (%s)" % (search_result["snippet"]["title"],
+                                   search_result["id"]["channelId"]))
+    elif search_result["id"]["kind"] == "youtube#playlist":
+      playlists.append("%s (%s)" % (search_result["snippet"]["title"],
+                                    search_result["id"]["playlistId"]))
+
+  print "Videos:\n", "\n".join(videos), "\n"
+  print "Channels:\n", "\n".join(channels), "\n"
+  print "Playlists:\n", "\n".join(playlists), "\n"
+
+
 if __name__ == '__main__':
-	youtube = get_authenticated_service()
+  youtube = get_authenticated_service()
 	# now you can do anything you want
+
+  searchByKeyword(youtube, "wpi")
+
+
+
+  
